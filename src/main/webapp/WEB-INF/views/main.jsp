0df237cecc0ff60bcbe563c27a4afd459bf60555
AnMin2000 <iededg@naver.com>
2024-05-30 01:12:43 +0900

    2024.05. 30 메인 페이지 만드는중

--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -18,6 +18,85 @@
     <img src="https://cdn-icons-png.flaticon.com/512/13611/13611575.png" class = "mainLogo" alt="My Image">
     <div class="text">Tell</div>
 </div>
+<div class="rounded-box"></div>
+
+<div class = "box">
+
+        <div class="outer">
+             <div class="inner-list">
+                 <div class="inner">
+                     <h2>first...</h2>
+                 </div>
+                 <div class="inner">
+                     <h2>second...</h2>
+                 </div>
+                 <div class="inner">
+                     <h2>third...</h2>
+                 </div>
+            </div>
+        </div>
+        <div class="outer2">
+            <div class="inner-list2">
+                <div class="inner2">
+                    <h2>first...</h2>
+                </div>
+                <div class="inner2">
+                    <h2>second...</h2>
+                </div>
+                <div class="inner2">
+                    <h2>third...</h2>
+                </div>
+            </div>
+        </div>
+    <div class="outer3">
+        <div class="inner-list3">
+            <div class="inner3">
+                <h2>first...</h2>
+            </div>
+            <div class="inner3">
+                <h2>second...</h2>
+            </div>
+            <div class="inner3">
+                <h2>third...</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="outer4">
+        <div class="inner-list4">
+            <div class="inner4">
+                <h2>first...</h2>
+            </div>
+            <div class="inner4">
+                <h2>second...</h2>
+            </div>
+            <div class="inner4">
+                <h2>third...</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="button-list">
+        <button class="button-left">← Left</button>
+        <button class="button-right">Right →</button>
+    </div>
+    <div class="button-list2">
+        <button class="button-left2">← Left</button>
+        <button class="button-right2">Right →</button>
+    </div>
+    <div class="button-list3">
+        <button class="button-left3">← Left</button>
+        <button class="button-right3">Right →</button>
+    </div>
+    <div class="button-list4">
+        <button class="button-left4">← Left</button>
+        <button class="button-right4">Right →</button>
+    </div>
+
+
+</div>
+
+
 <div id="sideMenu">
     <!-- 사이드 메뉴 내용 -->
     Side Menu Content
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -43,3 +43,211 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 50); // 50ms마다 반복하여 부드러운 애니메이션 효과를 적용
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+/*
+  div사이즈 동적으로 구하기
+*/
+const outer = document.querySelector('.outer');
+const innerList = document.querySelector('.inner-list');
+const inners = document.querySelectorAll('.inner');
+let currentIndex = 0; // 현재 슬라이드 화면 인덱스
+
+inners.forEach((inner) => {
+    inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+})
+
+innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
+
+/*
+  버튼에 이벤트 등록하기
+*/
+const buttonLeft = document.querySelector('.button-left');
+const buttonRight = document.querySelector('.button-right');
+
+buttonLeft.addEventListener('click', () => {
+    currentIndex--;
+    currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+    clearInterval(interval); // 기존 동작되던 interval 제거
+    interval = getInterval(); // 새로운 interval 등록
+});
+
+buttonRight.addEventListener('click', () => {
+    currentIndex++;
+    currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+    clearInterval(interval); // 기존 동작되던 interval 제거
+    interval = getInterval(); // 새로운 interval 등록
+});
+
+/*
+  주기적으로 화면 넘기기
+*/
+const getInterval = () => {
+    return setInterval(() => {
+        currentIndex++;
+        currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+    }, 2000);
+}
+
+let interval = getInterval(); // interval 등록
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    /*
+      div사이즈 동적으로 구하기
+    */
+    const outer = document.querySelector('.outer2');
+    const innerList = document.querySelector('.inner-list2');
+    const inners = document.querySelectorAll('.inner2');
+    let currentIndex = 0; // 현재 슬라이드 화면 인덱스
+
+    inners.forEach((inner) => {
+        inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+    })
+
+    innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
+
+    /*
+      버튼에 이벤트 등록하기
+    */
+    const buttonLeft = document.querySelector('.button-left2');
+    const buttonRight = document.querySelector('.button-right2');
+
+    buttonLeft.addEventListener('click', () => {
+        currentIndex--;
+        currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    buttonRight.addEventListener('click', () => {
+        currentIndex++;
+        currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    /*
+      주기적으로 화면 넘기기
+    */
+    const getInterval = () => {
+        return setInterval(() => {
+            currentIndex++;
+            currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+            innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+        }, 2000);
+    }
+
+    let interval = getInterval(); // interval 등록
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    /*
+      div사이즈 동적으로 구하기
+    */
+    const outer = document.querySelector('.outer3');
+    const innerList = document.querySelector('.inner-list3');
+    const inners = document.querySelectorAll('.inner3');
+    let currentIndex = 0; // 현재 슬라이드 화면 인덱스
+
+    inners.forEach((inner) => {
+        inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+    })
+
+    innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
+
+    /*
+      버튼에 이벤트 등록하기
+    */
+    const buttonLeft = document.querySelector('.button-left3');
+    const buttonRight = document.querySelector('.button-right3');
+
+    buttonLeft.addEventListener('click', () => {
+        currentIndex--;
+        currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    buttonRight.addEventListener('click', () => {
+        currentIndex++;
+        currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    /*
+      주기적으로 화면 넘기기
+    */
+    const getInterval = () => {
+        return setInterval(() => {
+            currentIndex++;
+            currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+            innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+        }, 2000);
+    }
+
+    let interval = getInterval(); // interval 등록
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    /*
+      div사이즈 동적으로 구하기
+    */
+    const outer = document.querySelector('.outer4');
+    const innerList = document.querySelector('.inner-list4');
+    const inners = document.querySelectorAll('.inner4');
+    let currentIndex = 0; // 현재 슬라이드 화면 인덱스
+
+    inners.forEach((inner) => {
+        inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+    })
+
+    innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
+
+    /*
+      버튼에 이벤트 등록하기
+    */
+    const buttonLeft = document.querySelector('.button-left4');
+    const buttonRight = document.querySelector('.button-right4');
+
+    buttonLeft.addEventListener('click', () => {
+        currentIndex--;
+        currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    buttonRight.addEventListener('click', () => {
+        currentIndex++;
+        currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
+        innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
+        clearInterval(interval); // 기존 동작되던 interval 제거
+        interval = getInterval(); // 새로운 interval 등록
+    });
+
+    /*
+      주기적으로 화면 넘기기
+    */
+    const getInterval = () => {
+        return setInterval(() => {
+            currentIndex++;
+            currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+            innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+        }, 2000);
+    }
+
+    let interval = getInterval(); // interval 등록
+});
